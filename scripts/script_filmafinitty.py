1fdc440dbcb7dce39990c90bc8cfa8f3432e81f8
Antonio <guezo983@gmail.com>
2016-07-04 20:17:21 +0200

    add search filmaffinitty

--- scripts/script_filmafinitty.py
+++ scripts/script_filmafinitty.py
@@ -9,14 +9,17 @@ from bs4 import BeautifulSoup
 #	print("Error connecting to database")
 
 idmovie = "859120" #star wars
-url = "https://www.filmaffinity.com/en/evideos.php?movie_id=" + idmovie
+#url = "https://www.filmaffinity.com/en/evideos.php?movie_id=" + idmovie
+url = "http://www.filmaffinity.com/es/search.php?stext=los+juegos+del+hambre&stype=all"
 response = urllib.request.urlopen(url)
 html = response.read().decode("utf8")
 soup = BeautifulSoup(html, 'html.parser')
 
 #soup.find_all('div')
-rating = soup.find(id="movie-rat-avg").get_text()
-count = soup.find(id="movie-count-rat").get_text()
-
-print(data)
+#rating = soup.find(id="movie-rat-avg").get_text()
+#count = soup.find(id="movie-count-rat").get_text()
+rating = soup.find_all("div",{"class":"ratcount-box"})[0].get_text()
+#count = soup.find(class="ratcount-box").get_text()
+print(rating)
+#print(count)
 ##movie-count-rat
