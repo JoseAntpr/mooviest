74f66e29941cb9dda7a853a67cec3890e31a5e93
Antonio <guezo983@gmail.com>
2016-07-05 16:51:17 +0200

    add save id_movie of filmaffinity and finish script meteoritic

--- scripts/script_filmaffinity.py
+++ scripts/script_filmaffinity.py
@@ -2,7 +2,7 @@ import urllib.request, json
 from bs4 import BeautifulSoup
 
 # film name of the film in spanish
-# return, The rating and th number ratings of the film
+# return, the rating and the number ratings of the film
 def rating_filmaffinity(film):
     search = film.replace(" ","+")
     url = "http://www.filmaffinity.com/es/search.php?stext="+search+"&stype=all"
@@ -10,18 +10,23 @@ def rating_filmaffinity(film):
     html = response.read().decode("utf8")
     soup = BeautifulSoup(html, 'html.parser')
 
+    id_str = ""
     rating_str = ""
     count_str = ""
     lista = soup.find_all("div",{"class":"avgrat-box"})
+
 
     if len(lista) > 0:
         rating_str = lista[0].get_text().strip()
         lista = soup.find_all("div",{"class":"ratcount-box"})
         count_str = lista[0].get_text().strip()
+        id_str = soup.find_all("div",{"class":"mc-title"})[0]
+        id_str = id_str.find_all("a")[0].get('href')
     else:
         try:
             rating_str = soup.find(id="movie-rat-avg").get_text().strip()
             count_str = soup.find(itemprop="ratingCount").get_text().strip()
+            id_str = soup.find_all("li",{"class":"active"})[0].find('a').get('href')
         except AttributeError:
             rating_str = "0"
             count_str = "0"
@@ -29,12 +34,13 @@ def rating_filmaffinity(film):
 
     rating = int(rating_str.replace(",", ""))
     count = int(count_str.replace(".", ""))
-
-    return rating,count
+    id_movie = int(id_str.replace("/es/film","").replace(".html",""))
+    return id_movie,rating,count
 
 #example, call function rating_filmaffinity
-r,c = rating_filmaffinity("E.T., el extraterrestre")
-#r,c = rating_filmaffinity("Los juegos del hambre")
+i,r,c = rating_filmaffinity("E.T., el extraterrestre")
+#i,r,c = rating_filmaffinity("Los juegos del hambre")
 
+print(i)
 print(r)
 print(c)
---
+++ scripts/script_metacritic.py
@@ -0,0 +1,45 @@
+import urllib.request, json
+from bs4 import BeautifulSoup
+
+# film name of the film in english
+# return, the id, the rating and the number ratings of the film
+def rating_metracritic(film):
+    search = film.replace(".","")
+    search = search.replace(",","")
+    search = search.replace(":","")
+    search = search.replace(" ","-")
+    url = "http://www.metacritic.com/movie/"+search.lower()
+    print(url)
+
+    response = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen(response).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    rating_str = "0"
+    count_str = "0"
+    lista = soup.find_all("div",{"class":"user"})
+    if len(lista) > 0:
+        rating_str = lista[0].get_text().strip()
+        lista = soup.find_all("div",{"class":"details side_details"})
+        lista = lista[0].find_all("span",{"class":"count"})
+        count_str = lista[0].find("a").get_text().lower()
+        count_str = count_str.replace("ratings","").strip()
+    else:
+        rating_str = "0"
+        count_str = "0"
+        print(film+" rating:_, count:_")
+
+    rating = int(rating_str.replace(".", "").replace(",", ""))
+    count = int(count_str.replace(".", "").replace(",", ""))
+
+    return search.lower(),rating,count
+
+
+
+
+#example, call function rating_metracritic
+i,r,c = rating_metracritic("E.T. The Extra-Terrestrial")
+
+print(i)
+print(r)
+print(c)
