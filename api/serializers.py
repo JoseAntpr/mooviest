d30e81b92db04758020da93e7cdd88de6738e9f5
Antonio <guezo983@gmail.com>
2016-07-12 12:47:15 +0200

    create serializer and viewsets

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,103 @@
-from movie.models import Movie
 from rest_framework import serializers
+from movie.models import Lang, Country, Celebrity, Celebrity_lang, Role, Role_lang, Saga, Saga_lang, Genre, Genre_lang, Emotion, Emotion_lang, Streaming, Source, Movie, Movie_lang, Rating, Catalogue, Catalogue_lang, Participation
 
-class MovieSerializer(serializers.HyperlinkedModelSerializer):
+
+class LangSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lang
+        fields = ('id', 'code')
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('id', 'lang', 'name')
+
+class CelebritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Celebrity
+        fields = ('id', 'name', 'born', 'image', 'twitter_account')
+
+class Celebrity_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Celebrity_lang
+        fields = ('id', 'celebrity', 'lang', 'biography', 'address', 'nationality')
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('id', 'langs', 'code')
+
+class Role_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role_lang
+        fields = ('id', 'role', 'lang', 'name')
+
+class SagaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Saga
+        fields = ('id', 'langs', 'code')
+
+class Saga_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Saga_lang
+        fields = ('id', 'saga', 'lang', 'name', 'synopsis')
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'langs', 'code')
+
+class Genre_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre_lang
+        fields = ('id', 'genre', 'lang', 'name')
+
+class EmotionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emotion
+        fields = ('id', 'langs', 'code')
+
+class Emotion_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emotion_lang
+        fields = ('id', 'emotion', 'lang', 'name', 'description')
+
+class StreamingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Streaming
+        fields = ('id', 'name', 'url')
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Source
+        fields = ('id', 'name')
+
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('title','synopsis','duration','released','country')
+        fields = ('id', 'genres', 'participations', 'langs', 'emotions', 'saga', 'catalogues', 'ratings', 'original_title', 'runtime', 'released', 'image', 'movie_producer', 'saga_order', 'average')
+
+class Movie_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie_lang
+        fields = ('id', 'movie', 'lang', 'country', 'title', 'synopsis')
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'source', 'movie', 'sourceid', 'rating', 'count')
+
+class CatalogueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Catalogue
+        fields = ('id', 'movie', 'streaming', 'langs')
+
+class Catalogue_langSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Catalogue_lang
+        fields = ('id', 'catalogue', 'lang', 'url', 'price')
+
+class ParticipationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participation
+        fields = ('id', 'celebrity', 'movie', 'role', 'character', 'award')
--- mooviest/urls.py
+++ mooviest/urls.py
@@ -15,18 +15,36 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from api.viewsets import LangViewSet, CountryViewSet, CelebrityViewSet, Celebrity_langViewSet, RoleViewSet, Role_langViewSet, SagaViewSet, Saga_langViewSet, GenreViewSet, Genre_langViewSet, EmotionViewSet, Emotion_langViewSet, StreamingViewSet, SourceViewSet, MovieViewSet, Movie_langViewSet, RatingViewSet, CatalogueViewSet, Catalogue_langViewSet, ParticipationViewSet
 from rest_framework.routers import DefaultRouter
-from api.views import MovieViewSet
-from django.views.generic import TemplateView
 
 router = DefaultRouter()
-router.register(r'movie',MovieViewSet)
+router.register(r'lang', LangViewSet)
+router.register(r'country', CountryViewSet)
+router.register(r'celebrity', CelebrityViewSet)
+router.register(r'celebrity_lang', Celebrity_langViewSet)
+router.register(r'role', RoleViewSet)
+router.register(r'role_lang', Role_langViewSet)
+router.register(r'saga', SagaViewSet)
+router.register(r'saga_lang', Saga_langViewSet)
+router.register(r'genre', GenreViewSet)
+router.register(r'genre_lang', Genre_langViewSet)
+router.register(r'emotion', EmotionViewSet)
+router.register(r'emotion_lang', Emotion_langViewSet)
+router.register(r'streaming', StreamingViewSet)
+router.register(r'source', SourceViewSet)
+router.register(r'movie', MovieViewSet)
+router.register(r'movie_lang', Movie_langViewSet)
+router.register(r'rating', RatingViewSet)
+router.register(r'catalogue', CatalogueViewSet)
+router.register(r'catalogue_lang', Catalogue_langViewSet)
+router.register(r'participation', ParticipationViewSet)
+
+
 
 urlpatterns = [
     url(r'^$', 'home.views.index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(router.urls)),
-    url(r'^api_auth/', include('rest_framework.urls',namespace='rest_framework')),
-    url(r'^nested_admin/', include('nested_admin.urls')),
-    url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^api/',include(router.urls)),
+    url(r'api_auth/',include('rest_framework.urls',namespace='rest_framework'))
 ]
