acd97ece7befd80d9b6eb21f39f412e10340e8aa
Antonio <guezo983@gmail.com>
2016-08-15 22:19:02 +0200

    Take out method get_soup, in to the interface, finish insert and update expert rating, missing update test

--- scripts/scrappers/script_metacritic.py
+++ scripts/scrappers/script_metacritic.py
@@ -1,24 +1,7 @@
 import urllib.request, json
 from bs4 import BeautifulSoup
+from . import interface
 
-# get_soup(url), return soup
-#
-#   Params
-#       - url, url that will scrapper
-#
-def get_soup(url):
-    error_code = False
-    error_message = ""
-    soup = ""
-    try:
-        response = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        html =  urllib.request.urlopen(response).read().decode("utf8")
-        soup = BeautifulSoup(html, 'html.parser')
-    except AttributeError:
-        error_code = True
-        error_message = "Error call \n"
-        soup = ""
-    return error_code, error_message, soup
 
 # get_url_metacritic_by_imdb(imdb), return metacritic url by imdb
 #
@@ -30,7 +13,7 @@ def get_url_metacritic_by_imdb(imdb):
     url = "http://www.imdb.com/title/"+imdb+"/criticreviews?ref_=tt_ov_rt"
     error_code = False
 
-    error_code, msg, soup = get_soup(url)
+    error_code, msg, soup = interface.get_soup(url)
     if error_code:
         error_message += msg
         return error_code, error_message
@@ -61,6 +44,7 @@ def format_params(rating, count):
     error_code = False
     error_message = ""
     try:
+        count = count.replace("ratings","").strip()
         rating = int(rating.replace(".", "").replace(",", ""))
         count = int(count.replace(".", "").replace(",", ""))
     except:
@@ -73,7 +57,7 @@ def format_params(rating, count):
 
 
 
-# get_rating(soup): sourceid returns, rating and count unformatted
+# get_rating(soup): sourceid returns, rating and count formatted
 #
 #   Params
 #       - soup, soup Metacritic movie page
@@ -90,14 +74,92 @@ def get_rating(soup):
             lista = soup.find_all("div",{"class":"details side_details"})
             lista = lista[0].find_all("span",{"class":"count"})
             count = lista[0].find("a").get_text().lower()
-            count = count.replace("ratings","").strip()
     except AttributeError:
         rating = ""
         count = ""
         error_code = True
         error_message = "Error get audience\n"
-    return error_code, error_message, rating, count
+    if error_code:
+        error_message += msg
+        return error_code, error_message
+    return format_params(rating, count)
 
+# get_rating_expert(soup): sourceid returns, rating and count unformatted
+#
+#   Params
+#       - soup, soup Metacritic movie page
+#
+def get_rating_expert(soup):
+    error_code = False
+    error_message = ""
+    rating = ""
+    count = ""
+    try:
+        lista = soup.find_all("div",{"class":"metascore_wrap highlight_metascore"})
+        if len(lista) > 0:
+            # print(lista)
+            rating = lista[0].find_all("span",{"itemprop":"ratingValue"})[0].get_text().strip()
+            count = lista[0].find_all("span",{"itemprop":"reviewCount"})[0].get_text().strip()
+    except AttributeError:
+        rating = ""
+        count = ""
+        error_code = True
+        error_message = "Error get expert\n"
+    if error_code:
+        error_message += msg
+        return error_code, error_message
+    return format_params(rating, count)
+
+def insert(db, movie_id, url, soup, expert):
+    error_code = False
+    error_message = ""
+    rating = 0
+    count = 0
+    name = "Metacritic"
+    if expert:
+        error_code, error_message, rating, count = get_rating_expert(soup)
+        name = "Metacritic Expert"
+    else:
+        error_code, error_message, rating, count = get_rating(soup)
+
+    if error_code:
+        error_message += msg
+        return error_code, error_message
+
+    params = json.dumps(
+        {
+            "source": db.SOURCES["Metacritic"],
+            "movie": movie_id,
+            "sourceid": url,
+            "name": "Metacritic",
+            "rating": rating,
+            "count": count
+        }
+    )
+    return error_code, error_message, db.insert_data(db.API_URLS["rating"], params)
+
+def update(db, rating_id, soup, expert):
+    error_code = False
+    error_message = ""
+    rating = 0
+    count = 0
+
+    if expert:
+        error_code, error_message, rating, count = get_rating_expert(soup)
+    else:
+        error_code, error_message, rating, count = get_rating(soup)
+
+    if error_code_expert:
+        error_message += msg
+        return error_code, error_message
+
+    params = json.dumps(
+        {
+            "rating": rating,
+            "count": count
+        }
+    )
+    return error_code, error_message, db.update_data(db.API_URLS["rating"]+str(rating_id)+"/", params)
 
 # insert_rating(db, movie_name, movie_id), insert rating, count and id movie
 #                  of Metacritic
@@ -113,32 +175,22 @@ def insert_rating(db, movie_id, imdb):
         error_message += msg
         return error_code, error_message
 
-    error_code, msg, soup = get_soup(url)
+
+    error_code, msg, soup = interface.get_soup(url)
     if error_code:
         error_message += msg
         return error_code, error_message
 
-    error_code, msg, rating, count = get_rating(soup)
+    error_code, msg, res = insert(db, movie_id, url, soup, False)
     if error_code:
         error_message += msg
-        return error_code, error_message
 
-    error_code, msg, rating, count = format_params(rating, count)
-    if error_code:
+    error_code_expert, msg, res_expert = insert(db, movie_id, url, soup, True)
+    if error_code_expert:
         error_message += msg
-        return error_code, error_message
 
-    params = json.dumps(
-        {
-            "source": db.SOURCES["Metacritic"],
-            "movie": movie_id,
-            "sourceid": url,
-            "name": "Metacritic",
-            "rating": rating,
-            "count": count
-        }
-    )
-    return db.insert_data(db.API_URLS["rating"], params)
+    return (error_code or error_code_expert), error_message, res, res_expert
+
 
 # update_rating(db, movie_name, movie_id), update rating
 #
@@ -147,32 +199,20 @@ def insert_rating(db, movie_id, imdb):
 #       - rating_id, id of rating
 #       - sourceid, url of Metacritic
 
-def update_rating(db, rating_id, sourceid):
-    error_message = "Rating id: " + str(rating_id) + " - Script rating Metacritic\n"
+def update_rating(db, rating_id, rating_expert_id, sourceid):
+    error_message = "Rating id: " + str(rating_id) +", rating_expert id: " + str(rating_expert_id) +" - Script rating Metacritic\n"
 
     error_code, msg, soup = get_soup(sourceid)
     if error_code:
         error_message += msg
         return error_code, error_message
 
-    error_code, msg, rating, count = get_rating(soup)
+    error_code, msg, res = update(db, movie_id, url, soup, False)
     if error_code:
         error_message += msg
-        return error_code, error_message
 
-    error_code, msg, rating, count = format_params(rating, count)
-    if error_code:
+    error_code_expert, msg, res_expert = update(db, movie_id, url, soup, True)
+    if error_code_expert:
         error_message += msg
-        return error_code, error_message
 
-    res = {}
-    if not error_code:
-        params = json.dumps(
-            {
-                "rating": rating,
-                "count": count
-            }
-        )
-        res = db.update_data(db.API_URLS["rating"]+str(rating_id)+"/", params)
-
-    return error_code, error_message, res
+    return (error_code or error_code_expert), error_message, res, res_expert
