289e4475ec0eb96ed399615bd912f807e0b9fddd
Antonio <guezo983@gmail.com>
2016-08-27 13:42:14 +0200

    call movie swipe finished

--- api/serializers_custom.py
+++ api/serializers_custom.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from movie.models import Movie, Movie_lang, Rating, Country, Genre_lang
+from movie.models import Movie, Movie_lang, Rating, Country, Genre, Genre_lang, Celebrity, Participation, Celebrity_lang
 
 class CountryAppSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,10 +22,38 @@ class Genre_LangAppSerializer(serializers.ModelSerializer):
         model = Genre_lang
         fields = ('id', 'genre', 'lang', 'name')
 
+class GenreAppSerializer(serializers.ModelSerializer):
+    langs = serializers.SerializerMethodField('get')
+    def get(self, obj):
+        lang = self.context['lang']
+        langs = Genre_lang.objects.filter(lang = lang, genre = obj)
+        langSerializer = Genre_LangAppSerializer(source='genre_lang_set', many=True, instance = langs)
+        return langSerializer.data
+    class Meta:
+        model = Genre
+        fields = ('id', 'langs', 'code')
+
+class Celebrity_langAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Celebrity_lang
+        fields = ('id', 'celebrity', 'lang', 'biography')
+
+class CelebrityAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Celebrity
+        fields = ('id', 'name', 'born', 'image', 'twitter_account', 'address')
+
+class ParticipationAppSerializer(serializers.ModelSerializer):
+    celebrity = CelebrityAppSerializer(many= False)
+    class Meta:
+        model = Participation
+        fields = ('celebrity', 'movie', 'role', 'character', 'award')
+
 class MovieAppSerializer(serializers.ModelSerializer):
     ratings = RatingAppSerializer(source='rating_set', many=True)
-    genres = serializers.SerializerMethodField('get_genres')
+    genres = GenreAppSerializer(many=True)
     langs = serializers.SerializerMethodField('get')
+    participations = serializers.SerializerMethodField('get_participation')
 
     def get(self, obj):
         lang = self.context['lang']
@@ -33,9 +61,11 @@ class MovieAppSerializer(serializers.ModelSerializer):
         langSerializer = Movie_LangAppSerializer(source='movie_lang_set', many=True, instance = langs)
         return langSerializer.data
 
-    def get_genres(self, obj):
+    def get_participation(self, obj):
         lang = self.context['lang']
-        genres = Genre_lang.objects.filter(lang=lang, movie= obj)
+        participations = Participation.objects.filter(movie = obj)
+        participationSerializer = ParticipationAppSerializer(source='participation_set', many=True, instance = participations)
+        return participationSerializer.data
 
     class Meta:
         model = Movie
