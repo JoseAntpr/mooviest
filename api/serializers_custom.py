e8219236052b6b80a8e80632a7873f62d3f05445
Jesús Garrido <jasus77@gmail.com>
2016-08-27 12:36:02 +0200

    get Genres

--- api/serializers_custom.py
+++ api/serializers_custom.py
@@ -1,24 +1,30 @@
 from rest_framework import serializers
-from movie.models import Movie, Movie_lang, Rating, Country
+from movie.models import Movie, Movie_lang, Rating, Country, Genre_lang
 
 class CountryAppSerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ('id', 'lang', 'name', 'code')
+        fields = ('name', 'code')
 
 class RatingAppSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ('id', 'name', 'rating', 'count', 'date_update')
+        fields = ('name', 'rating', 'count', 'date_update')
 
 class Movie_LangAppSerializer(serializers.ModelSerializer):
     country = CountryAppSerializer()
     class Meta:
         model = Movie_lang
-        fields = ('id', 'movie', 'lang', 'country', 'title', 'synopsis','image','trailer')
+        fields = ('id', 'lang', 'country', 'title', 'synopsis','image','trailer')
+
+class Genre_LangAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre_lang
+        fields = ('id', 'genre', 'lang', 'name')
 
 class MovieAppSerializer(serializers.ModelSerializer):
     ratings = RatingAppSerializer(source='rating_set', many=True)
+    genres = serializers.SerializerMethodField('get_genres')
     langs = serializers.SerializerMethodField('get')
 
     def get(self, obj):
@@ -26,6 +32,11 @@ class MovieAppSerializer(serializers.ModelSerializer):
         langs = Movie_lang.objects.filter(lang = lang, movie = obj)
         langSerializer = Movie_LangAppSerializer(source='movie_lang_set', many=True, instance = langs)
         return langSerializer.data
+
+    def get_genres(self, obj):
+        lang = self.context['lang']
+        genres = Genre_lang.objects.filter(lang=lang, movie= obj)
+
     class Meta:
         model = Movie
         fields = ('id', 'genres', 'participations', 'langs', 'emotions', 'ratings', 'original_title', 'runtime', 'released', 'movie_producer', 'saga_order', 'average')
