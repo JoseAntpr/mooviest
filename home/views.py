30f7e14e274b901241d3dcf67c37ca586240bc95
root <samuelhindley@gmail.com>
2016-08-31 13:49:25 +0200

    Movie info and translations succesfully fetched and pyc files removed

--- home/views.py
+++ home/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from movie.models import Movie, Movie_lang
+from movie.models import Movie, Movie_lang, Lang
 
 def index(request):
-    movies = Movie.objects.all()
-    return render(request, 'home/index.html', {'recommendations': movies})
-    #return render(request, 'home/index.html')
+    recommendations = getMovie(1, 'es')
+    return render(request, 'home/index.html', {'recommendations': recommendations})
 
-#def getRecommendations(request):
-#    movies = Movie.objects.all()
-#    return render(request, 'home/index.html', {'recommendations': movies})
+def getMovie(id, lang):
+    movie = Movie_lang.objects.select_related('movie').get(lang__code = lang, movie__id = id)
+    return movie
