e911ddacec5d2ea69dc89edfdb1fbc1db388c81c
Otto Garcia <octaviospain@gmail.com>
2016-10-25 11:29:50 +0200

    Changed to previous settings paths

--- mooviest/wsgi.py
+++ mooviest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mooviest.settings_deploy")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mooviest.settings_develop")
 
 application = get_wsgi_application()
