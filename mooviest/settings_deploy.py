27204eaa0c95279142bd24632e71493f3cf5de3b
Otto Garcia <octaviospain@gmail.com>
2016-10-25 11:26:47 +0200

    Changed settings paths for aws-deploy

--- mooviest/settings_deploy.py
+++ mooviest/settings_deploy.py
@@ -9,16 +9,16 @@ INSTALLED_APPS += (
 )
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
+}
 
 AWS_STORAGE_BUCKET_NAME = 'mooviest-storage'
 AWS_ACCESS_KEY_ID = 'AKIAIDXLX6RFS7RKVKOA'
